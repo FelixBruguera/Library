9ee98bad0e970c0bf43a129211d0f31fa861fc73
Felix Bruguera <felixbruguera0@gmail.com>
2024-07-25 15:57:53 -0400

    Add more validations, refactor functions and reorder the javascript file

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const booksDiv = document.querySelector('.books')
-const newBook = document.querySelector('#new-book')
-const form = document.querySelector('form')
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const formResponse = new FormData(document.querySelector('form'))
-    let book = new Book(formResponse)
-    lib.add(book)
-    form.reset()
-    dialog.close()
-})
-
-let dialog = {
-    modal: document.querySelector('dialog'),
-    exitButton: document.querySelector('#close-dialog'),
-    setup: function() {
-        newBook.addEventListener('click', () => this.modal.showModal())
-        this.exitButton.addEventListener('click', () => this.modal.close())
-    },
-    close: function() {
-        this.modal.close()
-    }
-}
+const exampleBooks = document.querySelector('#example-books')
 
 function newElement(elemType, elemContent, elemClass) {
     let elem = document.createElement(elemType)
     elem.textContent = elemContent
     elem.className = elemClass
     return elem
+}
+
+function getId(button) {
+    return button.parentElement.dataset.id
+}
+
+let dialog = {
+    modal: document.querySelector('dialog'),
+    exitButton: document.querySelector('#close-dialog'),
+    form: document.querySelector('form'),
+    button: document.querySelector('#new-book'),
+    close: function() {
+        this.modal.close()
+    },
+    addListeners: function() {
+        this.button.addEventListener('click', () => this.modal.showModal())
+        this.exitButton.addEventListener('click', () => this.modal.close())
+        this.form.addEventListener('submit', function(event) {
+            event.preventDefault()
+            let formResponse = new FormData(document.querySelector('form'))
+            lib.add( new Book(formResponse) )
+            this.reset()
+            dialog.close()
+        })
     }
+}
 
 function Library() {
     this.books = []
@@ -60,7 +63,7 @@ function Book(entries) {
     this.image = entries.get('image')
     this.id = lib.books.length+1
     this.updateRead = function() {
-        this.read = this.read == 'Read' ? 'Not read yet' : 'Read'
+        this.read = this.read == 'Read' ? 'Not read' : 'Read'
     }
     this.makeElements = function() {
         let title = newElement('h2', this.title, 'book-title')
@@ -91,35 +94,32 @@ function addListeners(book) {
         e.target.parentElement.classList.add('remove-book')
         lib.remove(bookElem)
         setTimeout(() => booksDiv.removeChild(e.target.parentElement), 450)
-        console.log(lib.books)
     })
 }
 
-
-dialog.setup()
-
-function getId(button) {
-    return button.parentElement.dataset.id
-}
-
-function transformResponse(response) {
-    let formData = new FormData()
-    formData.append('title', response.title)
-    formData.append('pages', response.pageCount)
-    formData.append('year', new Date(response.publishedDate).getFullYear())
-    formData.append('author', response.authors[0])
-    formData.append('read', 'Not read yet')
-    formData.append('image', response.imageLinks.thumbnail)
-    return formData
-}
-
-async function getRandomBooks() {
-    let response = await fetch('https://www.googleapis.com/books/v1/users/118196310033155438839/bookshelves/0/volumes')
-    data = await response.json()
-    data.items.forEach(item => lib.add(new Book(transformResponse(item.volumeInfo))))
+function googleBooksAPI() {
+    this.transformResponse = function (response) {
+        let formData = new FormData()
+        formData.append('title', response.title)
+        formData.append('pages', response.pageCount)
+        formData.append('year', new Date(response.publishedDate).getFullYear())
+        formData.append('author', response.authors[0])
+        formData.append('read', 'Not read')
+        formData.append('image', response.imageLinks.thumbnail)
+        return formData
     }
+
+    this.getRandomBooks = async function() {
+        let response = await fetch('https://www.googleapis.com/books/v1/users/106206099057354638675/bookshelves/0/volumes')
+        data = await response.json()
+        data.items.forEach(item => lib.add(new Book(this.transformResponse(item.volumeInfo))))
+    }
+}
+
 let lib = new Library()
-getRandomBooks()
+let api = new googleBooksAPI()
+dialog.addListeners()
+exampleBooks.addEventListener("click", () => api.getRandomBooks())
 
 
 
