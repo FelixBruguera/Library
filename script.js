c2085b929e2002638f70291c5db63b6e5dfed38f
Felix Bruguera <felixbruguera0@gmail.com>
2025-02-09 09:31:52 -0400

    Convert Library and Book to classes

--- script.js
+++ script.js
@@ -33,9 +33,11 @@ let dialog = {
     }
 }
 
-function Library() {
-    this.books = []
-    this.show = function(book) {
+class Library {
+    constructor() {
+        this.books = []
+    }
+    show(book) {
         let container = newElement('div', '', 'book')
         let elements = book.makeElements()
         for (let elem of elements) { container.appendChild(elem) }
@@ -44,28 +46,30 @@ function Library() {
         booksDiv.appendChild(container)
         addListeners(container)
    }
-   this.add = function(book) {
+   add(book) {
         this.books.push(book)
         this.show(book)
    }
-   this.remove = function(book) {
+   remove(book) {
         let index = this.books.indexOf(book)
         this.books.splice(index, 1)
    }
 }
 
-function Book(entries) {
-    this.title = entries.get('title');
-    this.pages = entries.get('pages');
-    this.year = entries.get('year');
-    this.author = entries.get('author');
-    this.read = entries.get('read');
-    this.image = entries.get('image')
-    this.id = lib.books.length+1
-    this.updateRead = function() {
+class Book extends Library{
+    constructor(entries) {
+        this.title = entries.get('title');
+        this.pages = entries.get('pages');
+        this.year = entries.get('year');
+        this.author = entries.get('author');
+        this.read = entries.get('read');
+        this.image = entries.get('image')
+        this.id = lib.books.length+1
+    }
+    updateRead() {
         this.read = this.read == 'Read' ? 'Not read' : 'Read'
     }
-    this.makeElements = function() {
+    makeElements() {
         let title = newElement('h2', this.title, 'book-title')
         let pages = newElement('p', this.pages+' Pages', 'book-pages')
         let year = newElement('p', this.year, 'book-year')
@@ -97,8 +101,8 @@ function addListeners(book) {
     })
 }
 
-function googleBooksAPI() {
-    this.transformResponse = function (response) {
+class googleBooksAPI {
+    transformResponse(response) {
         let formData = new FormData()
         formData.append('title', response.title)
         formData.append('pages', response.pageCount)
@@ -109,9 +113,9 @@ function googleBooksAPI() {
         return formData
     }
 
-    this.getRandomBooks = async function() {
+    async randomBooks() {
         let response = await fetch('https://www.googleapis.com/books/v1/users/106206099057354638675/bookshelves/0/volumes')
-        data = await response.json()
+        let data = await response.json()
         data.items.forEach(item => lib.add(new Book(this.transformResponse(item.volumeInfo))))
     }
 }
@@ -119,7 +123,7 @@ function googleBooksAPI() {
 let lib = new Library()
 let api = new googleBooksAPI()
 dialog.addListeners()
-exampleBooks.addEventListener("click", () => api.getRandomBooks())
+exampleBooks.addEventListener("click", () => api.randomBooks())
 
 
 
